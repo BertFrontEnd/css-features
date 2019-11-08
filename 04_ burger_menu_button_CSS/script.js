41860648ac74c83c0190b7c3067b4d5b6e18f637
BertFrontEnd <Berton@yandex.ru>
2019-11-08 16:24:18 +0300

    Update style.css & script.js

--- 04_ burger_menu_button_CSS/script.js
+++ 04_ burger_menu_button_CSS/script.js
@@ -3,5 +3,6 @@ const menuBtn = document.querySelector('.menu-btn');
 menuBtn.onclick = function(e) {
   e.preventDefault;
   menuBtn.classList.toggle("menu-btn_active");
+  menuBtn.classList.toggle("menu-btn_toggle");
   console.log(menuBtn);
 };
