572ace5fde0dd71e71e130895a8f16ecd131f69a
BertFrontEnd <Berton@yandex.ru>
2019-11-15 23:19:40 +0300

    Added Moving Burger menu

--- 04_burger_menu_button_CSS/script.js
+++ 04_burger_menu_button_CSS/script.js
@@ -1,7 +1,7 @@
 const menuBtn = document.querySelector('.menu-btn');
 
 menuBtn.onclick = function(e) {
-  e.preventDefault;
+  e.preventDefault();
   menuBtn.classList.toggle("menu-btn_active");
   menuBtn.classList.toggle("menu-btn_toggle");
   console.log(menuBtn);
---
+++ 07_moving_burger_menu/script.js
@@ -0,0 +1,12 @@
+const menuBtn = document.querySelector('.menu-btn');
+const menuBlock = document.querySelector(".menu-block");
+const myMenu = document.querySelector('.menu');
+
+menuBtn.onclick = function(e) {
+  e.preventDefault();
+  menuBtn.classList.toggle('menu-btn_active');
+  menuBlock.classList.toggle("menu-block_toggle");
+  myMenu.classList.toggle('menu_active');
+  console.log(menuBtn);
+  console.log(myMenu);
+};
