6dc10a0775157fc13461c042d3712b07c1ec87c4
BertFrontEnd <Berton@yandex.ru>
2019-12-26 22:16:03 +0300

    Update Blurred background

--- 18_blurred_background/script.js
+++ 18_blurred_background/script.js
@@ -1,9 +1,30 @@
 const block = document.querySelector('.block');
 
 block.onmousedown = function(e) {
-  window.addEventListener('mousemove', function() {
-    block.style.left = e.pageX + 'px';
-    block.style.top = e.pageY + 'px';
-    console.log(block);
-  });
+  let shiftX = e.pageX - getCoords(block).left;
+  let shiftY = e.pageY - getCoords(block).top;
+
+  moveAt(e);
+
+  function moveAt(e) {
+    block.style.left = e.pageX - shiftX + 'px';
+    block.style.top = e.pageY - shiftY + 'px';
+  }
+
+  document.onmousemove = function(e) {
+    moveAt(e);
+  };
+
+  block.onmouseup = function() {
+    document.onmousemove = null;
+    block.onmouseup = null;
+  };
+
+  function getCoords(elem) {
+    let box = elem.getBoundingClientRect();
+    return {
+      top: box.top + pageYOffset,
+      left: box.left + pageXOffset,
+    };
+  }
 };
